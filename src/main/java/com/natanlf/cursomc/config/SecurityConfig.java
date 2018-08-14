58ba4a225b5f5bf42391843f3b23a99d699944fc
Natan Lara Ferreira <natanlaraferreira@gmail.com>
2018-08-14 11:45:14 -0300

    Endpoints para buscar estados e cidades

--- src/main/java/com/natanlf/cursomc/config/SecurityConfig.java
+++ src/main/java/com/natanlf/cursomc/config/SecurityConfig.java
@@ -44,7 +44,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	private static final String[] PUBLIC_MATCHERS_GET = { //Acesso de leitura
 			"/produtos/**",
 			"/categorias/**",
-			"/clientes/**"
+			"/estados/**"
 	};
 	
 	private static final String[] PUBLIC_MATCHERS_POST = { //Acesso de leitura
