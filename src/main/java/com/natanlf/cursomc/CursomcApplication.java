6d410e32c3700528aa43761f4d3a5e302fb9edbf
Natan Lara Ferreira <natanlaraferreira@gmail.com>
2018-09-11 19:47:33 -0300

    Exportando War File

--- src/main/java/com/natanlf/cursomc/CursomcApplication.java
+++ src/main/java/com/natanlf/cursomc/CursomcApplication.java
@@ -3,9 +3,16 @@ package com.natanlf.cursomc;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class CursomcApplication implements CommandLineRunner {
+public class CursomcApplication extends SpringBootServletInitializer implements CommandLineRunner {
+	
+	@Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(CursomcApplication.class);
+    }
 	
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
